95616c20fbd2e540451ccb2db61f20fe5c38dd3e
Dídac Cayuela <didicayu@gmail.com>
2024-03-15 17:05:51 +0100

    attribute variants working
    still need to fix the deletion implementation

--- dev/odoo/mercadona-16.0/customer-addons/steam/models/partner.py
+++ dev/odoo/mercadona-16.0/customer-addons/steam/models/partner.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    game_ids = fields.Many2many('product.product', string="VideoGames")
+    games_ids = fields.Many2many('product.product', string="VideoGames")
 
     is_franchise = fields.Boolean(string='Is Franchise')
--- dev/odoo/mercadona-16.0/customer-addons/steam_subscription/models/product_template.py
+++ dev/odoo/mercadona-16.0/customer-addons/steam_subscription/models/product_template.py
@@ -21,34 +21,29 @@ class ProductTemplate(models.Model):
     @api.onchange('is_subscription')
     def _onchange_is_subscription(self):
         if self.is_subscription:
-            pass
-            # self.create_subscription_variants()
+            self.create_subscription_variants()
         else:
-            pass
-            # self.remove_subscription_variants()
+            self.remove_subscription_variants()
 
     def create_subscription_variants(self):
         subscription_attribute = self.env.ref('steam_subscription.product_subscription_attribute')
-        subscription_values = self.env['product.attribute.value'].search([('attribute_id', '=', subscription_attribute.id)])
+        subscription_values = self.env['product.attribute.value'].search(
+            [('attribute_id', '=', subscription_attribute.id)])
 
-        for value in subscription_values:
-            variant_name = f"{value.name} Subscription"
-            existing_variant = self.env['product.product'].search([('name', '=', variant_name)])
-            if not existing_variant:
-                variant_vals = {
-                    'name': variant_name,
-                    'product_tmpl_id': self.id,
-                    'product_template_attribute_value_ids': [(0, 0, {
-                        'product_attribute_value_id': value.id,
-                        'attribute_line_id': value.attribute_line_id.id,  # Set attribute_line_id
-                    })]
-                }
-                self.env['product.product'].create(variant_vals)
+        if not self.attribute_line_ids.attribute_id.ids:
+            attribute_line_vals = {
+                'product_tmpl_id': self.id.origin,
+                'attribute_id': subscription_attribute.id,
+                'value_ids': [(6, 0, subscription_values.ids)]
+            }
+            self.env['product.template.attribute.line'].create(attribute_line_vals)
 
     def remove_subscription_variants(self):
         subscription_variants = self.env['product.product'].search([
             ('product_tmpl_id', '=', self.id),
-            ('product_template_attribute_value_ids.attribute_id', '=', self.env.ref('steam_subscription.product_subscription_attribute').id)
+            ('product_template_attribute_value_ids.attribute_id', '=',
+             self.env.ref('steam_subscription.product_subscription_attribute').id),
+            ('product_template_attribute_value_ids.attribute_id.value_ids', '!=', False)
         ])
         subscription_variants.unlink()
 

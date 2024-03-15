b095b7f935ef73790e390d854338e60081693601
Dídac Cayuela <didicayu@gmail.com>
2024-03-15 15:43:02 +0100

    trying to add product variants

--- dev/odoo/mercadona-16.0/customer-addons/steam_subscription/models/product_template.py
+++ dev/odoo/mercadona-16.0/customer-addons/steam_subscription/models/product_template.py
@@ -21,7 +21,36 @@ class ProductTemplate(models.Model):
     @api.onchange('is_subscription')
     def _onchange_is_subscription(self):
         if self.is_subscription:
-            self.access_data()
+            pass
+            # self.create_subscription_variants()
+        else:
+            pass
+            # self.remove_subscription_variants()
+
+    def create_subscription_variants(self):
+        subscription_attribute = self.env.ref('steam_subscription.product_subscription_attribute')
+        subscription_values = self.env['product.attribute.value'].search([('attribute_id', '=', subscription_attribute.id)])
+
+        for value in subscription_values:
+            variant_name = f"{value.name} Subscription"
+            existing_variant = self.env['product.product'].search([('name', '=', variant_name)])
+            if not existing_variant:
+                variant_vals = {
+                    'name': variant_name,
+                    'product_tmpl_id': self.id,
+                    'product_template_attribute_value_ids': [(0, 0, {
+                        'product_attribute_value_id': value.id,
+                        'attribute_line_id': value.attribute_line_id.id,  # Set attribute_line_id
+                    })]
+                }
+                self.env['product.product'].create(variant_vals)
+
+    def remove_subscription_variants(self):
+        subscription_variants = self.env['product.product'].search([
+            ('product_tmpl_id', '=', self.id),
+            ('product_template_attribute_value_ids.attribute_id', '=', self.env.ref('steam_subscription.product_subscription_attribute').id)
+        ])
+        subscription_variants.unlink()
 
     def access_data(self):
         subscription_attribute = self.env.ref('steam_subscription.product_subscription_attribute')
